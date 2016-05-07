9a66be1a605a69b8d35951e4dc1d1e50a56e0082
bogdan <bogdan.gm24@gmail.com>
2016-05-07 17:03:26 +0300

    registation models in admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from tracker.models import User, Place, Tracker
+
+admin.site.register(User)
+admin.site.register(Place)
+admin.site.register(Tracker)
