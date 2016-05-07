713a542da577c6a10275587c07dc19ae2273b376
bogdan <bogdan.gm24@gmail.com>
2016-05-07 18:42:50 +0300

    add new views and templates

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 from tracker import views
 
 urlpatterns = [
+    url(r'^users/(?P<page>[0-9]+)/$', views.users, name='users_page'),
+    url(r'^users/$', views.users, name='users'),
+    url(r'^user_id_(?P<user_id>[0-9]+)/(?P<page>[0-9]+)$', views.user, name='user_page'),
+    url(r'^user_id_(?P<user_id>[0-9]+)/$', views.user, name='user'),
+    url(r'^tracker/(?P<track_id>[0-9]+)$', views.tracker, name='tracker'),
     url(r'^$', views.index, name='index'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,47 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from tracker.models import User, Tracker
 
 
 def index(request):
     context = {}
     return render(request, 'tracker/index.html', context)
+
+
+def users(request, page=1):
+    page = int(page)
+    users_by_id = User.objects.all()
+    p = Paginator(users_by_id, 2)
+    page1 = p.page(page)
+    context = {
+        'users': page1,
+        'paginator': p,
+        'page': page,
+    }
+    return render(request, 'tracker/users.html', context)
+
+
+def user(request, user_id, page=1):
+    user_id = int(user_id)
+    page = int(page)
+    user_by_id = Tracker.objects.all().filter(user_id=user_id)
+    name = User.objects.get(pk=user_id).first_name
+    p = Paginator(user_by_id, 2)
+    page1 = p.page(page)
+    context = {
+        'name': name,
+        'trackers': page1,
+        'paginator': p,
+        'page': page,
+        'user_id': user_id,
+    }
+    return render(request, 'tracker/user.html', context)
+
+
+def tracker(request, track_id):
+    track = Tracker.objects.get(pk=int(track_id))
+    context = {
+        'ob': track,
+    }
+    return render(request, 'tracker/tracker.html', context)
