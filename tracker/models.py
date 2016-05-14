99f608635cc97af6043a23de7ea1a6dfeedcea73
bogdan <bogdan.gm24@gmail.com>
2016-05-14 21:18:10 +0300

    add translate models and fields in admin

--- tracker/models.py
+++ tracker/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth import models as model_user
 
 
 class User(model_user.User):
-    position_lat = models.DecimalField(decimal_places=8, max_digits=10)
-    position_long = models.DecimalField(decimal_places=8, max_digits=10)
-    date_registration = models.DateTimeField(auto_now_add=True)
+    position_lat = models.DecimalField(decimal_places=8, max_digits=10, verbose_name="Широта")
+    position_long = models.DecimalField(decimal_places=8, max_digits=10, verbose_name="Довгота")
+    date_registration = models.DateTimeField(auto_now_add=True, verbose_name="Дата реєстрації")
+
+    class Meta:
+        verbose_name = 'Користувач'
+        verbose_name_plural = 'Користувачі'
 
     def __str__(self):
         return self.username
@@ -15,21 +19,29 @@ class User(model_user.User):
 
 
 class Place(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField(max_length=500)
-    position_lat = models.DecimalField(decimal_places=8, max_digits=10)
-    position_long = models.DecimalField(decimal_places=8, max_digits=10)
+    name = models.CharField(max_length=30, verbose_name="Назва")
+    description = models.TextField(max_length=500, verbose_name="Опис")
+    position_lat = models.DecimalField(decimal_places=8, max_digits=10, verbose_name="Широта")
+    position_long = models.DecimalField(decimal_places=8, max_digits=10, verbose_name="Довгота")
+
+    class Meta:
+        verbose_name = 'Місце'
+        verbose_name_plural = 'Місця'
 
     def __str__(self):
         return self.name
 
 
 class Tracker(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    date_create = models.DateTimeField(auto_now_add=True)
-    date_of_delivery = models.DateTimeField()
-    user_id = models.ForeignKey(User)
-    place_id = models.ForeignKey(Place)
+    name = models.CharField(max_length=30, unique=True, verbose_name="Назва")
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата створення")
+    date_of_delivery = models.DateTimeField(verbose_name="Необхідно виконати маршрут до:")
+    user_id = models.ForeignKey(User, verbose_name="Користувач")
+    place_id = models.ForeignKey(Place, verbose_name="Місце")
+
+    class Meta:
+        verbose_name = 'Маршрут'
+        verbose_name_plural = 'Маршрути'
 
     def __str__(self):
         return self.name
