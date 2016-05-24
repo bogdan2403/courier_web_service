4b72674adf7df11d3793312addff3a02bec4cb0f
bogdan <bogdan.gm24@gmail.com>
2016-05-25 00:25:06 +0300

    add registration

--- authorization/urls.py
+++ authorization/urls.py
@@ -3,4 +3,6 @@ from authorization import views
 urlpatterns = [
     url(r'^login/$', views.login),
     url(r'^logout/$', views.logout),
+    url(r'^registration/$', views.reg),
+    url(r'^reg_new_user/$', views.add_reg)
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import redirect, HttpResponse
+from django.shortcuts import redirect, HttpResponse, render
 from django.contrib import auth
+from authorization.forms import UserRegistrationForm
 
 
 # Create your views here.
@@ -20,3 +21,29 @@ def login(request):
             auth.login(request, user)
             return HttpResponse(1)
         return HttpResponse(0)
+
+
+def reg(request, errors=''):
+    # перервірка чи авторизований користувач
+    # авторизований не може потрапити на сторінку з реєстацією
+    if auth.get_user(request).username:
+        return redirect('/tracker/')
+    context = {
+        'form': UserRegistrationForm,
+        'errors': errors,
+    }
+    return render(request, 'authorization/reg.html', context=context)
+
+
+def add_reg(request):
+    if request.POST:
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.position_lat = 49.2339846
+            new_user.position_long = 28.4138335
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return redirect('/tracker/')
+        else:
+            return reg(request, form.errors)
