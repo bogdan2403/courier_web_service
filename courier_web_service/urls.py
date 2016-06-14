f43944251296ef9395231af89153c82af8abc61e
bogdan <bogdan.gm24@gmail.com>
2016-06-14 22:33:06 +0300

    add index for site/

--- courier_web_service/urls.py
+++ courier_web_service/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+import tracker.views as tracker
 
 urlpatterns = [
     url(r'^tracker/', include('tracker.urls')),
     url(r'^authorization/', include('authorization.urls')),
     url(r'^telephony/', include('telephony.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', tracker.index),
 ]
